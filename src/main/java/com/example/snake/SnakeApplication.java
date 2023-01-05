a481ba2621b18236077ca587e197e0c61ede07f4
abdmah <abdmah@student.chalmers.se>
2023-01-05 19:49:56 +0100

    Small changes on the options layout and the MIN_WINDOW_WIDTH + HEIGHT.

--- src/main/java/com/example/snake/SnakeApplication.java
+++ src/main/java/com/example/snake/SnakeApplication.java
@@ -17,6 +17,10 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class SnakeApplication extends Application {
 
   private static final int MIN_WINDOW_WIDTH = 690;
